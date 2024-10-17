7ad37d8ef60b72c9f4e65a44cc31cbb22113bc2c
ConradoFrasson <conradox12@gmail.com>
2024-10-17 19:10:19 -0300

    12

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ import mongoose from "mongoose";
 
 (async () => {
     try {
-        await mongoose.connect()
+        await mongoose.connect(process.env.MONGODB)
 
     } catch (error) {
         console.log(error);
